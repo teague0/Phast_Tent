343d89f4a6e9e0a6bc63de6a004d818f3ad46af8
Edward Hurme <edwardhurme@gmail.com>
2022-04-11 15:06:32 -0500

    get foraging sites and plot rasters for each location

--- R/MergePlanetRasters.R
+++ R/MergePlanetRasters.R
@@ -2,20 +2,31 @@
 
 library(raster)
 library(rgdal)
+library(rgeos)
 
-files <- list.files("H:/Bocas_2022_03_07_psscene_analytic_sr_udm2/files/", pattern = "*harmonized_clip.tif", 
-                    full.names = TRUE)
-Bocas <- list()
-for(i in 1:length(files)){
-  Bocas[i] <- stack(files[i])    
-}
+# turn off factors
+options(stringsAsFactors = FALSE)
 
-m <- do.call(merge, Bocas)
+# files <- list.files("H:/Bocas_2022_03_07_psscene_analytic_sr_udm2/files/", pattern = "*harmonized_clip.tif", 
+#                     full.names = TRUE)
+# Bocas <- list()
+# for(i in 1:length(files)){
+#   Bocas[i] <- stack(files[i])    
+# }
+# 
+# m <- do.call(merge, Bocas)
+
+# save(m, file = "H:/Bocas_2022_03_07_psscene_analytic_sr_udm2/bocas.robj")
+m <- brick("../../../Dropbox/MPI/Phyllostomus/Fieldwork/Data/Bocas.tif")
 plot(m)
-save(m, file = "H:/Bocas_2022_03_07_psscene_analytic_sr_udm2/bocas.robj")
 
-?raster::merge
+names(m)
+#par(col.axis = "white", col.lab = "white", tck = 0)
+plotRGB(m,
+        r = 3, g = 2, b = 1,
+        stretch = "lin",
+        axes = TRUE,
+        main = "RGB composite image\n Landsat Bands 4, 3, 2")
+box(col = "white")
 
-plot(Bocas[[1]])
-plotRGB(Bocas[[1]], 
-        r = 3, g = 2, b = 1, scale = 263)
+
