4e22cd7384342065f560e9974edc1f59f502b774
Edward Hurme <edwardhurme@gmail.com>
2022-05-04 10:47:34 +0200

    where to save bocas tif

--- R/MergePlanetRasters.R
+++ R/MergePlanetRasters.R
@@ -12,6 +12,10 @@ for(i in 1:length(files)){
 
 m <- do.call(merge, Bocas)
 plot(m)
+
+outfile <- writeRaster(m, filename='../../../Dropbox/MPI/Phyllostomus/Fieldwork/Data/Bocas.tif', 
+                       format="GTiff", overwrite=TRUE,options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
+
 save(m, file = "H:/Bocas_2022_03_07_psscene_analytic_sr_udm2/bocas.robj")
 
 ?raster::merge
